c7c74d91511d5095b50811fdc25342de3a85c3c0
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-09-24 18:57:31 +0530

    [Umesh3103]Added: Checking exact winning position

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -69,3 +69,36 @@ public class SnakeLadderUC4{
 		}
         }
 }
+
+// Checking for exact winning position
+
+public class SnakeLadderUC5{
+	public static void main(String[] args){
+        	int START_POSITION =0;
+        	int NO_PLAY=0, LADDER =1, SNAKE=2;
+		int Player_Position=START_POSITION;
+
+        	while(Player_Position <= 100){
+        		int Die_Number= (int) (Math.floor(Math.random() *10) %6)+1;
+        	
+        		int Check_Option = (int) (Math.floor(Math.random() *10) %3);
+        	
+                	if(Player_Position < 0){
+                        	Player_Position= START_POSITION;
+                	}
+			else if(Player_Position == 100){
+				break;
+			}
+                	else if(Check_Option == LADDER){
+                        	Player_Position += Die_Number;
+				if(Player_Position >100){
+					Player_Position -= Die_Number;
+				}
+                	}
+                	else if(Check_Option ==SNAKE){
+                        	Player_Position -= Die_Number;
+                	}
+        	}
+        
+	}
+}
