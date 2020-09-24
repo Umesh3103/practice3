78f35cd4a6477b40ddb12e435769ddc5aa9c47c6
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-09-24 18:00:18 +0530

    [Umesh3103]Added: Checking for ladders and snake options

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -20,3 +20,25 @@ public class SnakeLadderUC2{
 		int n =(int) (Math.floor(Math.random() *10) %6)+1;
 	}
 }
+
+// Player checks for multiple options
+
+public class SnakeLadderUC3{
+	public static void main(String[] args){
+		int START_POSITION =0;
+		int Die_Number= (int) (Math.floor(Math.random() *10) %6)+1;
+		int NO_PLAY=0, LADDER =1, SNAKE=2;
+		int Check_Option = (int) (Math.floor(Math.random() *10) %3);
+		int Player_Position=0;
+		
+		if(Check_Option == NO_PLAY){
+			Player_Position= Player_Position;
+		}
+		else if(Check_Option == LADDER){
+			Player_Position = Player_Position + Die_Number;
+		}
+		else{
+			Player_Position = Player_Position - Die_Number;
+		}
+	}
+}
